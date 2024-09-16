f3706b9c84efab41e6cf960ea66ee4fb649f9b50
yash1701garg <yashgarg1701@gmail.com>
2024-09-16 18:48:22 +0530

    project initialization

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+ 
+
+  return (
+  <div>
+    Hello Jiii
+    <Button>Submit</Button>
+  </div>
+  )
+}
+
+export default App
