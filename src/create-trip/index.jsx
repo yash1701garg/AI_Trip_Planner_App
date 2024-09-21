6e6e68ed398b92b3c0384406d89f52cb14be2fc2
Varun Tyagi <varuntyagi0099@gmail.com>
2024-09-21 19:40:43 +0530

    h2andpchnages

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const CreateTrip = () => {
+function CreateTrip ()  {
   return (
-    <div>CreateTrip</div>
+    <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
+        <h2 className='font-bold text-3xl'>Tell us your travel preferences</h2>
+        <p className='mt-3 text-gray-500 text-xl'>Just provide some basic information, and our trip planner will generate a customized itinerary based on your preferences.</p>
+    </div>
   )
 }
 
