7ba6c86e5be2fc2b7794a8336c00a0565f6761fe
Varun Tyagi <varuntyagi0099@gmail.com>
2024-09-21 19:50:29 +0530

    what is destinatiom

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -5,6 +5,11 @@ function CreateTrip ()  {
     <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
         <h2 className='font-bold text-3xl'>Tell us your travel preferences</h2>
         <p className='mt-3 text-gray-500 text-xl'>Just provide some basic information, and our trip planner will generate a customized itinerary based on your preferences.</p>
+        <div className='mt-20'>
+         <div>
+          <h2 className='text-xl my-3 font-medium'>What is destination of choice?</h2>
+         </div>
+        </div>
     </div>
   )
 }
