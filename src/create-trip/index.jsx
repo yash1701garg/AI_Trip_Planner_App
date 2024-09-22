0c581aeb6c5f44ec1565c33ac1a09876d93eaadd
Varun Tyagi <varuntyagi0099@gmail.com>
2024-09-22 12:31:02 +0530

    api

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
-function CreateTrip ()  {
+// function CreateTrip ()
+const CreateTrip = () => {
   return (
     <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
         <h2 className='font-bold text-3xl'>Tell us your travel preferences</h2>
@@ -9,9 +11,8 @@ function CreateTrip ()  {
          <div>
           <h2 className='text-xl my-3 font-medium'>What is destination of choice?</h2>
           <GooglePlacesAutocomplete
-          apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
-          
-          />
+        apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY} // Using the API key from .env
+      />
          </div>
         </div>
     </div>
