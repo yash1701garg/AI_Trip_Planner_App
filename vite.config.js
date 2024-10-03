5083c26ebdae984465e2aa9e471aee9fa1038bb5
yash1701garg <yashgarg1701@gmail.com>
2024-10-03 10:55:46 +0530

    kya likhu ab

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'; // Import defineConfig from 'vite'
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/AI_Trip_Planner_App/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
