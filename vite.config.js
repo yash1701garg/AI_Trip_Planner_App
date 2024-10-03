e06b817f79e7bf85b6df82b6003df50cfa5eee11
yash1701garg <yashgarg1701@gmail.com>
2024-10-03 10:33:36 +0530

    under deployement

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: '/AI_Trip_Planner_App/', // Ensure this matches your repo name
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    outDir: 'build',
+  },
+});
